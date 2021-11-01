0ebcb8eb14142d7824fae1b0d86b8b815805686e
BhoomikaShastry <62304213+BhoomikaShastry@users.noreply.github.com>
2021-11-01 17:42:41 +0530

    Add files via upload

--- Test1/FlowerDemo.java
+++ Test1/FlowerDemo.java
@@ -7,41 +7,38 @@ public class FlowerDemo {
 	{
 		System.out.println(continent);
 		System.out.println("----------------------------------");
+		HashMap<String,String> h=new HashMap<>();
 		if(continent.equalsIgnoreCase("Asia"))
 		{
-			HashMap<String,String> h1=new HashMap<>();
-			h1.put("India\t", "\tLotus");
-			h1.put("Pakistan ", "\tJasmine");
-			h1.put("Nepal\t", "\tLali Gurans");
+			
+			h.put("India\t", "\tLotus");
+			h.put("Pakistan ", "\tJasmine");
+			h.put("Nepal\t", "\tLali Gurans");
 			System.out.println("Country Name \t National Flower");
 			System.out.println("----------------------------------");
-			Set set=h1.entrySet();
-			for(Object data:set)
-			System.out.println(data);
+			for(String data:h.keySet())
+			System.out.println(data+" "+h.get(data));
 		}
 		else if(continent.equalsIgnoreCase("Europe"))
 		{
-			HashMap<String,String> h2=new HashMap<>();
-			h2.put("Germany\t", "\tCorn Flower");
-			h2.put("France\t", "\tLily");
-			h2.put("Greece\t", "\tAcanthus Mollis");
+			
+			h.put("Germany\t", "\tCorn Flower");
+			h.put("France\t", "\tLily");
+			h.put("Greece\t", "\tAcanthus Mollis");
 			System.out.println("Country Name \t National Flower");
 			System.out.println("----------------------------------");
-			Set set=h2.entrySet();
-			for(Object data:set)
-			System.out.println(data);
+			for(String data:h.keySet())
+				System.out.println(data+" "+h.get(data));
 			}
 		else if(continent.equalsIgnoreCase("Africa"))
 		{
-			HashMap<String,String> h3=new HashMap<>();
-			h3.put("Egypt\t", "\tBlue Lotus");
-			h3.put("Nigeria\t", "\tYellow Trumpet");
-			h3.put("Kenya\t", "\tLali Orchid");
+			h.put("Egypt\t", "\tBlue Lotus");
+			h.put("Nigeria\t", "\tYellow Trumpet");
+			h.put("Kenya\t", "\tLali Orchid");
 			System.out.println("Country Name \t National Flower");
 			System.out.println("----------------------------------");
-			Set set=h3.entrySet();
-			for(Object data:set)
-			System.out.println(data);
+			for(String data:h.keySet())
+				System.out.println(data+" "+h.get(data));
 			}
 		else
 			System.out.println("Enter any one of the 3 continents");
