5530f320aee4ee8c711012a120bfbc4e4ab64c8e
BhoomikaShastry <62304213+BhoomikaShastry@users.noreply.github.com>
2021-11-01 17:43:25 +0530

    Add files via upload

--- Test2/Test2.java
+++ Test2/Test2.java
@@ -6,7 +6,6 @@ public class Test2 {
 		
 		Scanner s=new Scanner(System.in);
 		EnjoyRide er=new EnjoyRide();
-		do {
 		System.out.println("Enter the no. of Adults");
 		int n1=s.nextInt();
 		System.out.println("Enter the no. of Children");
@@ -20,26 +19,28 @@ public class Test2 {
 		{
 		case 1: int adultfare1=50;
 						int childfare1=40;
+						System.out.println("Ride: Alibaba");
 						er.calculate(n1, n2, h, childfare1, adultfare1);
 						break;
 		case 2: int adultfare2=60;
 							 int childfare2=40;
+							 System.out.println("Ride: Alpine Slide");
 							 er.calculate(n1, n2, h, childfare2, adultfare2);
 							 break;
 		case 3: int adultfare3=45;
 		 					 int childfare3=25;
+		 					 System.out.println("Ride: Bumper Cars");
 		 					 er.calculate(n1, n2, h, childfare3, adultfare3);
 		 					 break;
 		case 4: int adultfare4=70;
 							 int childfare4=60;
+							 System.out.println("Ride: Balloon Race");
 							 er.calculate(n1, n2, h, childfare4, adultfare4);
 							 break;
-		case 5: System.exit(0);
 		default: System.out.println("Enter number between 1 to 5");
 				 break;
 		
 		}
-		}while(true);
 		
 	}
 
